8c7e37278a42653a4c64539dc696afcf23dd571a
jj <137390275+ojjc@users.noreply.github.com>
2023-12-29 18:09:33 -0500

    Update import_modules.py

--- import_modules.py
+++ import_modules.py
@@ -8,5 +8,5 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import MultiLabelBinarizer
-import tensorflow as transform
-from tensorflow import keras
+# import tensorflow as tf
+# from tensorflow import keras
